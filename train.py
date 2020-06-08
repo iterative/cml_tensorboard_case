23797c4558f2ddbbce064759e8849a92e3203eda
Elle O'Brien <andronovhopf@gmail.com>
2020-06-08 15:49:21 -0700

    Increase dropout

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ def create_model():
   return tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(10, activation='softmax')
   ])
 
