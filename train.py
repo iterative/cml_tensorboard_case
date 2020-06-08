bd9fa19bd02431c2bb857a0c00d808d6fd96c69f
Elle O'Brien <andronovhopf@gmail.com>
2020-06-08 15:43:26 -0700

    decrease neurons in hidden layer

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 def create_model():
   return tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10, activation='softmax')
   ])
